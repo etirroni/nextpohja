fb810f0b2b9ec15341453a7cd9d2a3866441d1e1
Eelis Tirroniemi <124739749+MatkallaPankkiin@users.noreply.github.com>
2024-04-21 22:47:04 +0300

    cooma#3

--- my-app/components/NavBar.js
+++ my-app/components/NavBar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styled from "styled-components";
-import Painter from "./Painter";
+import BrushIconDataURL from "@/icons/BrushIcon";
 
 const NavDiv = styled.div`
   display:flex;
@@ -12,13 +12,37 @@ const NavDiv = styled.div`
   z-index:2;
   border-radius:10px;
 `
+const StyledLink = styled.div`
+  padding: 2rem; /* Equivalent to p-4 in Tailwind */
+  text-decoration: none; /* Override any default text-decoration */
+  color: #ffffff; /* Equivalent to text-gray-100 in Tailwind */
+  text-shadow: 2px 6px 20px #E1E1E1; /* Equivalent to shadow-gray-300 in Tailwind */
+  background-color: transparent; /* If there's a background color set, adjust accordingly */
+  cursor: none; /* Equivalent to cursor-none in Tailwind */
+  transition: color 0.2s ease-in, transform 0.2s ease-in, text-decoration 0.2s ease-in; /* Transition properties */
+  
+  &:hover {
+    color: #fec3df; 
+    scale:110%; 
+    text-shadow: 2px 6px 20px #0000;
+    text-decoration: underline wavy; 
+  }
+`;
 export default function NavBar() {
     return (
       <NavDiv>
-        <Link className="[text-shadow:_2px_6px_20px_var(--tw-shadow-color)] text-gray-100 shadow-gray-300 px-4 rounded-lg cursor-none hover:text-black hover:scale-110 transition ease-in duration-200" href={'/'}>Home</Link>
-        <Link className="[text-shadow:_2px_6px_20px_var(--tw-shadow-color)] text-gray-100 shadow-gray-300 px-4 rounded-lg cursor-none hover:text-white transition ease-in duration-200" href={'/projects'}>Projects</Link>
-        <Link className="[text-shadow:_2px_6px_20px_var(--tw-shadow-color)] text-gray-100 shadow-gray-300 px-4 rounded-lg cursor-none hover:text-white transition ease-in duration-200" href={'/contact'}>Contact</Link>
-        <Link className="[text-shadow:_2px_6px_20px_var(--tw-shadow-color)] text-gray-100 shadow-gray-300 px-4 rounded-lg cursor-none hover:text-white transition ease-in duration-200" href={'/Gallery'}>MISC</Link>
+        <StyledLink>
+          <Link style={{ cursor: `url('${BrushIconDataURL}'), auto` }} href={'/'}>Home</Link>
+        </StyledLink>
+        <StyledLink>
+          <Link style={{ cursor: `url('${BrushIconDataURL}'), auto` }}  href={'/projects'}>Projects</Link>
+        </StyledLink>
+        <StyledLink>
+          <Link style={{ cursor: `url('${BrushIconDataURL}'), auto` }}  href={'/contact'}>Contact</Link>
+        </StyledLink>
+        <StyledLink>
+          <Link style={{ cursor: `url('${BrushIconDataURL}'), auto` }}  href={'/Gallery'}>Gallery</Link>
+        </StyledLink>        
       </NavDiv>
     );
     
